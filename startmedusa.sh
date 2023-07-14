816ac4c3a4a94c38df35fd8f2f744f2f7c2a1aa2
Heiko Köppen <github@avenqo.com>
2023-07-14 07:53:01 +0200

    Update startmedusa.sh

--- startmedusa.sh
+++ startmedusa.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# ---------- Debug Infos--------
+echo "--- Working directory ---"
+pwd
+echo "--- Files in PWD ---"
+ls -l
+npm --version
+yarn --version
 
+echo "======== Starting Server =========="
+cd /app/shop/my-medusa-store
+medusa develop
+
+echo "======== DONE: Starting App  =========="
+
+# just in case that somethings fails -> start interactive session by "docker exec -it medusa-app /bin/bash "
+while true; do sleep 1; done
